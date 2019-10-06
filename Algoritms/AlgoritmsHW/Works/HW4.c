4a8886a12ad08c24a8db5306d1a71fbae4eb3408
Ivan <wolfmanr@mail.ru>
2019-10-06 16:24:21 +0300

    завершил 1 задачу

--- Algoritms/AlgoritmsHW/Works/HW4.c
+++ Algoritms/AlgoritmsHW/Works/HW4.c
@@ -37,52 +37,73 @@ void HW4_main(){
  *  * Number of routes with obstacles. Implement reading an array with an obstacle and finding the number of routes.
 */
 
-#define wall 1
+#define wall 0
 
 int routes(int x, int y) {
   if (x == 0 || y == 0) return 1;
   else return routes(x - 1, y) + routes(x, y - 1);
 }
 
-void HW4_Task1(){
-	printf("Setup Size of checkboard");
-	int* field;
-	int cols;
-	printf("Enters Columns: ");
-	scanf("%d",&cols);
-	int rows;
-	printf("\nEnters Rows: ");
-	scanf("%d",&rows);
-	field=malloc(rows*cols*sizeof(*field));
+int routes2(int x, int y, int* arr, int cols) {
+	if(*(arr+(y*cols+x)) == wall) return 0;
+	else{
+		if (x == 0 || y == 0) return 1;
+		else return routes2(x - 1, y,arr,cols) + routes2(x, y - 1,arr,cols);
+	}
+}
 
+void HW4_Task1(){
+
+	printf("Setup Size of checkboard\n");
+	int* field;
+	int cols = 7;
+	int rows = 7;
+	printf("Enter Columns: ");
+	scanf("%d",&cols);
+	printf("Enter Rows: ");
+	scanf("%d",&rows);
+
+	field=malloc(rows*cols*sizeof(*field));
 	for (int var = 0; var < rows*cols; ++var) {
-		*(field+var)=0;
+		*(field+var)=1;
 	}
 
 	printf("Enter a number of blocks: ");
 	int blockNums;
 	scanf("%d",&blockNums);
-	printf("Enter blocks of path (x is col, y is row)");
+	printf("Enter blocks of path (x is col, y is row)\n");
 
 	for (int block = 0; block < blockNums; ++block) {
 		int x;
 		int y;
-		printf("\nEnter x: ");
+		printf("Enter x: ");
 		scanf("%d",&x);
 		printf("Enter y: ");
 		scanf("%d",&y);
 		*(field+((y-1)*cols+x-1))=wall;
+		puts("");
 	}
 
+	/*    											//Print Default CheckBoard with Blocks
 	for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
 				printf("%7d ", *(field+(i*cols+j)));
 			}
 			printf("\n");
-		}
+	}
 	puts("");
-	for (int i = 0; i < 7; i++) {
-		for (int j = 0; j < 7; j++) {
+	*/
+
+	for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				printf("%7d ", routes2(j,i,field,cols));
+			}
+			printf("\n");
+	}
+	puts("");
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
 			printf("%7d ", routes(i, j));
 		}
 		printf("\n");
