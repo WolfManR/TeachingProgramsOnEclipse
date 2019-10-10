8a09c56715887c183815f9557226ac1e62fbee66
Ivan <wolfmanr@mail.ru>
2019-10-11 00:05:35 +0300

    начал разработку 3 задачи

--- Algoritms/AlgoritmsHW/Works/HW5.c
+++ Algoritms/AlgoritmsHW/Works/HW5.c
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 
 #define TCount 3
+#define T char
+
 
 void HW5_Task1();
 void HW5_Task2();
@@ -55,8 +57,6 @@ void HW5_Task1(){
  *  * Create a function that copies a simply connected list
  *  (that is, it creates a copy of a simply connected list in memory without deleting the first list).
 */
-#define T char
-
 typedef struct Node{
 	T data;
 	struct Node *next;
@@ -72,7 +72,7 @@ void initList(List *list){
 	list->size = 0;
 }
 
-
+//////////////////////////////////////////////////////////////////
 boolean push(List *lt, T value) {
   Node *temp = (Node*) malloc(sizeof(Node));
   if (temp == NULL)
@@ -153,6 +153,120 @@ void HW5_Task2(){
  *  3.
  *  * Implement a queue based on a doubly linked list.
 */
-void HW5_Task3(){
 
+typedef struct element{
+	T data;
+	struct element *next;
+	struct element *prev;
+}Element;
+
+typedef struct{
+	Element *front;
+	Element *back;
+	int size;
+}Deque;
+
+void initDeque(Deque *deq){
+	deq->front = NULL;
+	deq->back = NULL;
+	deq->size = 0;
+}
+
+boolean pushBack(Deque *deq, T value){
+	Element *temp = (Element*) malloc(sizeof(Element));
+	if (temp == NULL)
+		return false;
+	//if(deq->back != NULL)
+	//	deq->back->next = temp;
+
+	temp->data = value;
+	temp->next = NULL;
+	if(deq->back == NULL && deq->front == NULL){
+			free(deq->front);
+			deq->front = temp;
+			temp->prev = NULL;
+		}
+	else
+		temp->prev = deq->back;
+
+	deq->back = temp;
+	deq->size++;
+	return true;
+}
+
+boolean pushFront(Deque *deq, T value){
+	Element *temp = (Element*) malloc(sizeof(Element));
+	if (temp == NULL)
+		return false;
+	//if(deq->front != NULL)
+	//	deq->front->next = temp;
+
+	temp->data = value;
+	temp->prev = NULL;
+	if(deq->back == NULL && deq->front == NULL){
+		free(deq->back);
+		deq->back = temp;
+		temp->next = NULL;
+	}
+	else
+		temp->next = deq->front;
+
+	deq->front = temp;
+	deq->size++;
+	return true;
+}
+
+T popBack(Deque *deq){
+	if (deq->size == 0) {
+		return -1;
+	}
+
+	Element *temp = deq->back;
+	T result = deq->back->data;
+
+	deq->back = deq->back->prev;
+	deq->size--;
+	free(temp);
+	return result;
+}
+
+T popFront(Deque *deq){
+	if (deq->size == 0) {
+		return -1;
+	}
+
+	Element *temp = deq->front;
+	T result = deq->front->data;
+
+	deq->front = deq->front->next;
+	deq->size--;
+	free(temp);
+	return result;
+}
+
+void readDeqFromBack(Deque *deq){
+	while (deq->size > 0) {
+		printf("%c ", popBack(deq));
+	}
+	printf("\n");
+}
+
+void readDeqFromFront(Deque *deq){
+	while (deq->size > 0) {
+		printf("%c ", popFront(deq));
+	}
+	printf("\n");
+}
+
+void HW5_Task3(){
+	Deque deq;
+	initDeque(&deq);
+
+	pushBack(&deq, 'T');
+	pushBack(&deq, 'f');
+	pushBack(&deq, 'B');
+	pushBack(&deq, 'd');
+	pushBack(&deq, 'c');
+	printf("size: %d\n", deq.size);
+	readDeqFromBack(&deq);
 }
