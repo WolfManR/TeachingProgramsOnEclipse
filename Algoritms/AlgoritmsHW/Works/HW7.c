c9c1eeea9886ee2cd73e62e7abb362a09bfb979e
Ivan <wolfmanr@mail.ru>
2019-10-17 21:46:59 +0300

    добавил очередь, динамический массив и графф

--- Algoritms/AlgoritmsHW/Works/HW7.c
+++ Algoritms/AlgoritmsHW/Works/HW7.c
@@ -32,6 +32,76 @@ void HW7_main(){
 	TaskMenu(tasks,TCount);
 }
 
+typedef struct DynamicArray{
+	struct GraphNode* graphs;
+	int size;
+	int capacity;
+}DynArray;
+
+typedef struct GraphNode{
+	int data;
+	int used;
+	DynArray *children;
+}GraphNode;
+
+void initDynArray(DynArray *arr){
+	arr->size = 0;
+	arr->graphs = calloc(1, sizeof(*arr->graphs));
+	arr->capacity = 1;
+}
+
+void Expand(DynArray * arr){
+	arr->graphs = realloc(arr->graphs, (arr->capacity * 2) * sizeof(*arr->graphs));
+	if(arr->graphs != NULL) arr->capacity *= 2;
+}
+
+void Add(DynArray* arr, struct GraphNode *Node){
+	if(arr->size < arr->capacity){
+		arr->graphs[arr->size] = *Node;
+		arr->size++;
+	}
+	else{
+		Expand(arr);
+		arr->graphs[arr->size] = *Node;
+		arr->size++;
+	}
+}
+
+typedef struct Node{
+	GraphNode graph;
+	struct Node *next;
+}Node;
+
+typedef struct Queue{
+	Node *first;
+	Node *last;
+	int size;
+}Queue;
+
+void initQueue(Queue *que){
+	que->first = NULL;
+	que->last = NULL;
+	que->size = 0;
+}
+
+void enqueue(Queue *que, GraphNode *graph){
+	Node *temp = malloc(sizeof(*temp));
+	temp->graph = *graph;
+	if(que->last == NULL){
+		que->first = que->last = temp;
+	}
+	else{
+		que->last->next = temp;
+		que->last = temp;
+	}
+}
+
+void dequeue(Queue *que){
+	if(que->first != NULL)
+		que->first = que->first->next;
+	else
+		que->last = NULL;
+}
 
 /*//////////////////////////////////////////////////////////////////////////////////////
  *  1.
